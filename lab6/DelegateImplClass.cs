b9721c7ff695865669ad0bb8f61d0960b663b059
Vasiliy Dmitriev <devasidmi@gmail.com>
2017-12-06 10:47:59 +0300

    lab6 done

---
+++ lab6/DelegateImplClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6
+{
+    static class DelegateImplClass
+    {
+
+        public delegate int mDelegate(double x, int y);
+
+        public static int mainMethod(double x, int y)
+        {
+            return Convert.ToInt32(Math.Pow(x,3)) + Convert.ToInt32(x) + y;
+        }
+
+        public static void DelegateMethod(double x, int y, mDelegate del)
+        {
+            Console.WriteLine(del(x, y));
+        }
+
+        public static void DelegateFunc(double x, int y, Func<double,int,int> del)
+        {
+            Console.WriteLine(del(Convert.ToInt32(x), y));
+        }
+    }
+}
---
+++ lab6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+
+            // метод, соответствующий данному делегату
+            DelegateImplClass.mDelegate mDelegate = new DelegateImplClass.mDelegate(DelegateImplClass.mainMethod);
+            Console.WriteLine(mDelegate(4.0, 2));
+
+            //метод, принимающий разработанный Вами делегат, в качестве
+            //одного из входным параметров (метод+лямбда)
+            DelegateImplClass.DelegateMethod(3.0, 3,mDelegate);
+            DelegateImplClass.DelegateMethod(6.0, 8, (x,y) => Convert.ToInt32(Math.Pow(x, 3)) + Convert.ToInt32(x) + y);
+
+            //обобщенный делегат Func<>,соответствующий сигнатуре
+            //разработанного делегата
+            DelegateImplClass.DelegateFunc(10.0, 1500,DelegateImplClass.mainMethod);
+            DelegateImplClass.DelegateFunc(4.0, 200, (x, y) => Convert.ToInt32(Math.Pow(x, 3)) + Convert.ToInt32(x) + y);
+        }
+    }
+}
